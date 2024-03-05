dc786ef095952ead8a6ad9bcc7c69f35107ed8b8
trspusta <115706989+trspusta@users.noreply.github.com>
2024-03-05 14:26:33 +0800

    Update submitgrades.js

--- AlcaidePusta_Milestone1_Final/AlcaidePusta_Milestone1_Final/submitgrades.js
+++ AlcaidePusta_Milestone1_Final/AlcaidePusta_Milestone1_Final/submitgrades.js
@@ -1,7 +1,9 @@
 const students = [
     {
         code: 136194,
-        studname: "Alcaide, Hannah Bea Reyes",
+        studname: "ALCAIDE, HANNAH BEA REYES",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
         final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
         grd: "NEW",
         yearlvl: 2,
@@ -10,8 +12,10 @@ const students = [
         entryyrlvl: 1
     },
     {
-        code: 136194,
-        studname: "Año, Jesus Ricardo Tejano",
+        code: 128852,
+        studname: "ANO, JESUS RICARDO TEJANO",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
         final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
         grd: "NEW",
         yearlvl: 3,
@@ -20,8 +24,10 @@ const students = [
         entryyrlvl: 1
     },
     {
-        code: 136194,
-        studname: "Alcaide, Hannah Bea Reyes",
+        code: 142104,
+        studname: "ARAPOC, BEATRICE JAN GOMEZ",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
         final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
         grd: "NEW",
         yearlvl: 2,
@@ -29,6 +35,150 @@ const students = [
         yrofentry: 2,
         entryyrlvl: 1
     },
+    {
+        code: 133118,
+        studname: "BELEN, HALEY MACKENZIE UBALDE",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 144272,
+        studname: "DAGATAN, TRISTAN KYLE MALNEGRO",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 142744,
+        studname: "DOBLI, FERDINAND JOHN FRANCISCO",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 129322,
+        studname: "LABRADOR, THONY DANIELLE ALIBANGBANG",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 143804,
+        studname: "LANAJA,  CHRIS LAWRENCE JUAN",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 132703,
+        studname: "MARIFLOR, RIZAL KENT EXCOBAR",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 141743,
+        studname: "NATIVIDAD, KARL JUSTIN ANGELO SALON",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 142608,
+        studname: "OPERIO, RAPHAEL MIGUEL DIZON",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 146083,
+        studname: "PIMENTEL, JOB AARON TAN",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 137565,
+        studname: "PUSTA, TISHA RYLE SAYMAN",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 128848,
+        studname: "QUIANZON, EDWARD ANTHONY RIVAS",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    },
+    {
+        code: 129749,
+        studname: "VENERACION, ISIAH GILMORE BUENAFLOR",
+        prelim: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        midterm: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"],
+        final: ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"], // Dropdown options
+        grd: "NEW",
+        yearlvl: 2,
+        acadstatus: "Regular",
+        yrofentry: 2,
+        entryyrlvl: 1
+    }
 ];
 
 const studentTable = document.getElementById('student-table').getElementsByTagName('tbody')[0];
@@ -37,13 +187,14 @@ students.forEach(student => {
     const newRow = studentTable.insertRow();
     Object.keys(student).forEach(key => {
         const newCell = newRow.insertCell();
-        if (key === 'final') {
+        if (key === 'prelim' || key === 'midterm' || key === 'final') {
             const select = document.createElement('select');
-            student[key].forEach(option => {
+            ["A", "B+", "B", "C+", "C", "D", "F", "FD", "WP"].forEach(option => {
                 const optionElement = document.createElement('option');
                 optionElement.textContent = option;
                 select.appendChild(optionElement);
             });
+            select.value = student[key]; // Set default value
             newCell.appendChild(select);
         } else {
             newCell.textContent = student[key];
